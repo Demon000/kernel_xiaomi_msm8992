1c3a1467f48aec53fb59720f973675c3fe62bf27
Demon000 <demonsingur@gmail.com>
2017-02-12 23:23:11 +0200

    drivers: synaptics_dsx_libra: symlink /proc/touchscreen to input device

--- drivers/input/touchscreen/synaptics_dsx_libra/synaptics_dsx_core.c
+++ drivers/input/touchscreen/synaptics_dsx_libra/synaptics_dsx_core.c
@@ -19,6 +19,7 @@
  */
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/proc_fs.h>
 #include <linux/slab.h>
 #include <linux/interrupt.h>
 #include <linux/delay.h>
@@ -864,6 +865,30 @@ static ssize_t synaptics_rmi4_virtual_key_map_show(struct kobject *kobj,
 	}
 
 	return count;
+}
+
+static ssize_t synaptics_rmi4_proc_init(struct kobject *sysfs_node_parent) {
+	int ret = 0;
+	char *driver_path;
+
+	struct proc_dir_entry *proc_entry_ts;
+
+	driver_path = kzalloc(PATH_MAX, GFP_KERNEL);
+	if(driver_path) {
+		sprintf(driver_path, "/sys%s", 
+			kobject_get_path(sysfs_node_parent, GFP_KERNEL));
+	}
+
+	proc_entry_ts = proc_symlink("touchscreen", NULL, driver_path);
+	if(proc_entry_ts == NULL) {
+		pr_err("%s: Couldn't symlink to touchscreen\n", __func__);
+	}
+
+	printk("driver_path: %s\n", driver_path);
+
+	kfree(driver_path);
+
+	return ret;
 }
 
 static int synaptics_rmi4_f11_abs_report(struct synaptics_rmi4_data *rmi4_data,
@@ -3863,6 +3888,8 @@ static int synaptics_rmi4_probe(struct platform_device *pdev)
 			goto err_sysfs;
 		}
 	}
+
+	synaptics_rmi4_proc_init(&rmi4_data->input_dev->dev.kobj);
 
 	rmi4_data->rb_workqueue =
 			alloc_ordered_workqueue("dsx_rebuild_workqueue", WQ_HIGHPRI);
